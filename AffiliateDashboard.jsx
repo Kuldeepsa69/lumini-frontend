6a85c952f1e67bba48f9cf4494300b3bc9a7a443
Kuldeepsa69 <Kuldeeps89175@gmail.com>
2025-07-23 01:55:32 +0530

    Add files via upload

---
+++ AffiliateDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AffiliateDashboard = () => {
+  return (
+    <div>
+      <h2>Affiliate Dashboard</h2>
+      <p>This is your dashboard to manage affiliate products.</p>
+    </div>
+  );
+};
+
+export default AffiliateDashboard;
---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AffiliateDashboard from './AffiliateDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Lumini</h1>
+      <AffiliateDashboard />
+    </div>
+  );
+}
+
+export default App;
